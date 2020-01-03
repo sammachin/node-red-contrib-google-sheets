d5029180a9375c0cf14bcc9644511800e50db4c1
Sam Machin <sam@sammachin.com>
2020-01-03 22:13:06 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# node-red-contrib-google-sheets
+
+A basic node for reading and writing from/to google sheets.
+
+## Auth
+
+To setup auth this node uses a google service account:
+
+Create a new service account from [This Page](https://console.cloud.google.com/iam-admin/serviceaccounts?_ga=2.184919274.-272657095.1578084478)
+
+Download a JSON credentials object for the service account.
+
+Give that account access to the sheets API.
+
+Share your sheet with the email address of the service account eg `nodered@nodered-12345.iam.gserviceaccount.com`
+
+## Sheets
+
+The sheet ID can be found in the URL of your google sheet, for example in
+`https://docs.google.com/spreadsheets/d/1UuVIH2O38XK0TfPMGHk0HG_ixGLtLk6WoBKh4YSrDm4/edit#gid=0`
+
+The ID would be `1UuVIH2O38XK0TfPMGHk0HG_ixGLtLk6WoBKh4YSrDm4`
+
+## Cells
+Google sheets uses the follwing syntax to reference a tab and cells of the worksheet
+
+The format is `Sheet1!A1:C3`
+
+Where `Sheet1` is the Sheet name followed by a `!` then the grid of the first cell eg `A1` then a `:` and finally the grid of the last cell eg `C£`
+
+A range of cells can be a Row at `A1:A5`, a Column `A1:E1` or even a block such as `A1:C3`
+
---
+++ gsheet.js
@@ -0,0 +1,146 @@
+let {google} = require('googleapis');
+
+module.exports = function(RED) {
+    function gSheet(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.creds = JSON.parse(RED.nodes.getNode(config.creds).credentials.creds);
+        this.method = config.method;
+        node.on('input', function(msg) {
+          if(!config.sheet){
+            this.sheet = msg.sheet
+          } else {
+            this.sheet = config.sheet;
+          }
+          if(!config.cells){
+            this.cells = msg.topic
+          } else {
+            this.cells = config.cells;
+          }
+			    let jwtClient = new google.auth.JWT(
+			       this.creds.client_email,
+			       null,
+			       this.creds.private_key,
+			       ['https://www.googleapis.com/auth/spreadsheets',
+			        'https://www.googleapis.com/auth/drive']
+			     );
+           let sheet = this.sheet;
+           let cells = this.cells;
+           let method = this.method;
+           let sheets = google.sheets('v4');
+			     jwtClient.authorize(function (err, tokens) {
+			       if (err) {
+			         node.error("Auth Error: " +err);
+			         return;
+			       } else {
+               switch(method) {
+                 case 'get':
+    				       sheets.spreadsheets.values.get({
+    				         auth: jwtClient,
+    				         spreadsheetId: sheet,
+    				         range: cells
+    				       }, function (err, response) {
+    				         if (err) {
+    						       node.error('The API returned an error: ' + err, msg);
+    				         } else {
+    						       msg.payload = flatten(response.data.values);
+    						       node.send(msg);
+    				         }
+    			         });
+                   break;
+                 case 'append':
+                   if (Array.isArray(msg.payload)){
+                     if (Array.isArray(msg.payload[0])){
+                       msg.payload = {"values": msg.payload}
+                     } else {
+                       msg.payload = {"values": [msg.payload]}
+                     }   
+                   } else{
+                     msg.payload = {"values": [[msg.payload]]}
+                   }
+    				       sheets.spreadsheets.values.append({
+    				         auth: jwtClient,
+    				         spreadsheetId: sheet,
+    				         range: cells,
+                     valueInputOption: "USER_ENTERED",
+                     insertDataOption: "INSERT_ROWS",
+                     resource: msg.payload
+    				       }, function (err, response) {
+    				         if (err) {
+    						       node.error('The API returned an error: ' + err, msg);
+    				         } else {
+    						       msg.payload = response.data.updates;
+    						       node.send(msg);
+    				         }
+    			         });
+                   break;
+                 case 'update':
+                   if (Array.isArray(msg.payload)){
+                     if (Array.isArray(msg.payload[0])){
+                       msg.payload = {"values": msg.payload}
+                     } else {
+                       msg.payload = {"values": [msg.payload]}
+                     }   
+                   } else{
+                     msg.payload = {"values": [[msg.payload]]}
+                   }
+    				       sheets.spreadsheets.values.update({
+    				         auth: jwtClient,
+    				         spreadsheetId: sheet,
+    				         range: cells,
+                     valueInputOption: "USER_ENTERED",
+                     resource: msg.payload
+    				       }, function (err, response) {
+    				         if (err) {
+    						       node.error('The API returned an error: ' + err, msg);
+    				         } else {
+    						       msg.payload = response.data
+    						       node.send(msg);
+    				         }
+    			         });
+                   break;
+                 case 'clear':
+    				       sheets.spreadsheets.values.clear({
+    				         auth: jwtClient,
+    				         spreadsheetId: sheet,
+    				         range: cells,
+    				       }, function (err, response) {
+    				         if (err) {
+    						       node.error('The API returned an error: ' + err, msg);
+    				         } else {
+    						       msg.payload = response.data;
+    						       node.send(msg);
+    				         }
+    			         });
+                  break;             
+               } 
+             }
+			     });
+        });
+  }
+  
+  
+  
+  function gauth(n){
+     RED.nodes.createNode(this, n);
+     this.creds = n.creds;
+  }
+  RED.nodes.registerType("GSheet",gSheet);
+  RED.nodes.registerType("gauth",gauth,{
+   credentials: {
+     creds: {type:"text"}
+   }
+  });  
+}
+
+function flatten(arr) {
+  if (arr.length == 1 && arr[0].length == 1){
+      return arr[0][0];
+  } else if (arr.length != 1 && arr[0].length == 1){
+    return arr.reduce((acc, val) => acc.concat(val), []);
+  } else if (arr.length == 1 && arr[0].length != 1){
+    return arr.reduce((acc, val) => acc.concat(val), []);
+  } else{
+    return arr
+  }
+}
